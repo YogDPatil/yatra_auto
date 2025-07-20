92144f79d4a5b9db49bc7cdbf0b233e6556d4877
YogDPatil <ydpatil.it@gmail.com>
2025-07-20 18:23:47 +0530

    code update to check browser

--- src/test/java/com/ui/pages/HomePage.java
+++ src/test/java/com/ui/pages/HomePage.java
@@ -2,19 +2,28 @@ package com.ui.pages;
 
 import com.utils.BrowserUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
 
 public final class HomePage extends BrowserUtils {
 
+    private WebDriver driver;
+
     private static final By EMAIL_FIELD_LOCATOR = By.id("mobile-number");
     private static final By LOGIN_BUTTON_LOCATOR = By.xpath("//button[contains(text(),'Login')]");
+    private static final By PASS_FIELD_LOCATOR = By.id("pass");
 
     public HomePage(WebDriver driver) {
         super(driver);
+        this.driver = driver;
     }
 
     public void login() {
         enterText(EMAIL_FIELD_LOCATOR, "yogeshpatil.om23@gmail.com");
+        clickOn(LOGIN_BUTTON_LOCATOR);
+        enterText(PASS_FIELD_LOCATOR, "Testing@123");
         clickOn(LOGIN_BUTTON_LOCATOR);
     }
 }
--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -3,16 +3,25 @@ package com.ui.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public abstract class TestBase {
     protected WebDriver driver;
 
+    @Parameters("browser")
     @BeforeMethod(alwaysRun = true)
-    public void initialiseDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void initialiseDriver(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
         driver.get("https://www.yatra.com/");
         driver.manage().window().maximize();
     }
@@ -24,7 +33,7 @@ public abstract class TestBase {
                 Thread.sleep(2000);
                 driver.quit();
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException ignore) {
 
         }
     }
